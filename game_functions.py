5e7d102b9e7f42baf656868b42949e3df29993c9
yelimkoo <43474141+yelimkoo@users.noreply.github.com>
2018-09-21 07:07:10 -0700

    Update game_functions.py

--- game_functions.py
+++ game_functions.py
@@ -95,12 +95,12 @@ def create_alien(ai_settings, screen, aliens, alien_number):
     alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
     aliens.add(alien)
 
-    def create_fleet(ai_settings, screen, aliens):
-        """Create a full fleet of aliens."""
-        number_aliens_x = get_number_aliens_x(ai_settings, alien.rect.width)
-        number_rows = get_number_rows(ai_settings, ship.rect.height, alien.rect.height)
+def create_fleet(ai_settings, screen, aliens):
+     """Create a full fleet of aliens."""
+     number_aliens_x = get_number_aliens_x(ai_settings, alien.rect.width)
+     number_rows = get_number_rows(ai_settings, ship.rect.height, alien.rect.height)
 
-        for row_number in range(number_rows):
+      for row_number in range(number_rows):
             for alien_number in range(number_aliens_x):
                 create_alien(ai_settings, screen, aliens, alien_number, row_number)
 
